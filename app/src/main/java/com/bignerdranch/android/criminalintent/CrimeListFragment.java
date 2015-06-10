c16cedc7e12592973c0f185c385fcbde64067462
Dylan House <dhouse08@gmail.com>
2015-06-10 09:25:25 -0700

    List fragments now reload displayed data on activity resume

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -70,4 +70,11 @@ public class CrimeListFragment extends ListFragment {
 
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        ((CrimeAdapter)getListAdapter()).notifyDataSetChanged();
+    }
+
+
 }
